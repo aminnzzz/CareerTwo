719baa8063bc7526d3ccdbf5fd76cc0b8ea5192d
Amin Nazemzadeh <amin.nazemzadeh96@gmail.com>
2025-04-02 00:18:51 -0400

    optionals

--- Swift/optionalEasier.playground/Contents.swift
+++ Swift/optionalEasier.playground/Contents.swift
@@ -15,3 +15,22 @@ let users = [
 ]
 
 let userNames = users.map(\.?.name)
+dump(userNames)
+
+for case let user? in users {
+    print(user.name)
+}
+
+let from = users[0]
+let to = users[1]
+
+switch (from, to) {
+case let (from?, to?):
+    print("Transferring from \(from.name) to \(to.name).")
+case let (from?, nil):
+    print("Transfer from \(from.name) needs a destination.")
+case let (nil, to?):
+    print("Transfer to \(to.name) needs a source.")
+case (nil, nil):
+    print("Transfer is missing both source and destination.")
+}
