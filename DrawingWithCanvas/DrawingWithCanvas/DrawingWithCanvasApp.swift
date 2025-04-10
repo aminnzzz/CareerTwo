653db7f9d5acf5f1797fab19bdc87585323e054b
Amin Nazemzadeh <amin.nazemzadeh96@gmail.com>
2025-04-10 12:10:16 -0400

    drawing canvas

---
+++ DrawingWithCanvas/DrawingWithCanvas/DrawingWithCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingWithCanvasApp.swift
+//  DrawingWithCanvas
+//
+//  Created by amin nazemzadeh on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DrawingWithCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
